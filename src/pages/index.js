59084bbd57e484af9a37e6f1d1db04b0a1e739da
Martin Conde <martin@martinconde.com>
2021-10-31 14:15:38 +0700

    try lazy hydrate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react"
 import loadable from '@loadable/component'
+import LazyHydrate from "react-lazy-hydration"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,20 +11,20 @@ import Row from "../components/Blocks/row"
 import IconBox from '../components/Blocks/iconBox'
 import FaveGyms from "../components/faveGyms"
 
-const InterMap = React.lazy(() => import('../components/thaiMap'))
-
+// const InterMap = React.lazy(() => import('../components/thaiMap'))
+const InterMap = loadable(() => import('../components/thaiMap'))
 
 export default function Home({ data }) {
-  const [scroll, setScroll] = useState(false)
+  // const [scroll, setScroll] = useState(false)
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 200)
-    })
-    return function cleanup() {
-      setScroll(true)
-    }
-  }, [])
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     setScroll(window.scrollY > 200)
+  //   })
+  //   return function cleanup() {
+  //     setScroll(true)
+  //   }
+  // }, [])
 
   return (
     <Layout>
@@ -64,7 +65,7 @@ export default function Home({ data }) {
 
       
         <BgRow fullWidth withPadding>
-          {typeof window !== 'undefined' && (
+          {/* {typeof window !== 'undefined' && (
             
             <Suspense fallback={<div>Loading..</div>}>
               {scroll &&
@@ -72,7 +73,11 @@ export default function Home({ data }) {
             }
             </Suspense>
             
-          )}
+          )} */}
+
+          <LazyHydrate whenVisible>
+          <InterMap />
+          </LazyHydrate>
         </BgRow>
       
     </Layout>
